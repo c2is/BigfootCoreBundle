b4a7e8b052da1245d78035b8b431224228d9c2c6
sebastien.huot <sebastien.huot@c2is.fr>
2013-07-31 11:05:41 +0200

    Update custom.js

--- Resources/public/js/custom.js
+++ Resources/public/js/custom.js
@@ -201,7 +201,7 @@ $(function() {
 
         $translatedFields.parent().parent().prepend(Twig.render(localeTabs, {locales: locales, currentLocale: currentLocale, basePath: basePath}));
 
-        var $localeTab = $('#locale-tabs');
+        var $localeTab = $('.locale-tabs');
         $localeTab.on('click', 'a', function(event) {
             event.stopPropagation();
 
@@ -214,6 +214,8 @@ $(function() {
                 $(this).addClass('active');
                 currentLocale = newLocale;
             }
+
+            return false;
         });
     }
 })
