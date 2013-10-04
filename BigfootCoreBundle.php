ef8ea7f06a1e8591328af0fa754b8aa569ada733
Guillaume Manen <g.manen@c2is.fr>
2013-10-04 17:08:58 +0200

    Removing Point type for DQL

--- BigfootCoreBundle.php
+++ BigfootCoreBundle.php
@@ -18,9 +18,5 @@ class BigfootCoreBundle extends Bundle
     public function boot()
     {
         $this->container->get('bigfoot.route_manager')->addBundle($this->getName());
-
-        Type::addType('point', 'Bigfoot\Bundle\CoreBundle\ORM\PointType');
-        $em = $this->container->get('doctrine.orm.bigfoot_entity_manager');
-        $em->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('point', 'point');
     }
 }
