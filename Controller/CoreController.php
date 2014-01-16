097d0785df1389fa817bba183fd2875bef2f6b15
Acrack <yehya.latti@gmail.com>
2014-01-16 12:23:34 +0100

    extends BaseController

--- Controller/CoreController.php
+++ Controller/CoreController.php
@@ -4,8 +4,6 @@ namespace Bigfoot\Bundle\CoreBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\DependencyInjection\ContainerAware;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
@@ -15,13 +13,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
  *
  * @Cache(maxage="0", smaxage="0", public="false")
  */
-class CoreController extends ContainerAware
+class CoreController extends BaseController
 {
     /**
      * @Route("/", name="admin_home")
      */
     function homeAction()
     {
-        return new Response($this->container->get('twig')->render(sprintf('%s::base.html.twig', $this->container->getParameter('bigfoot.theme.bundle')), array()), 200);
+        return $this->render(sprintf('%s::base.html.twig', $this->container->getParameter('bigfoot.theme.bundle')), array());
     }
 }
