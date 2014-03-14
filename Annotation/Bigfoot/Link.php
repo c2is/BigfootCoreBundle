9821a07ff6197ee6b14ed3680b848f712ce4bac1
Acrack <yehya.latti@gmail.com>
2014-03-14 15:08:44 +0100

    Nav

---
+++ Annotation/Bigfoot/Link.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bigfoot\Bundle\CoreBundle\Annotation\Bigfoot;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/**
+ * @Annotation
+ * @Target("PROPERTY")
+ */
+final class Link extends Annotation
+{
+
+}
