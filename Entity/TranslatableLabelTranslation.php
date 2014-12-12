c652023d4e239d6b1c62e9f385b7d30f3ef45e38
sebastien.huot <huotseb@gmail.com>
2014-12-12 12:34:12 +0100

    Refactoring : postgre compliance

--- Entity/TranslatableLabelTranslation.php
+++ Entity/TranslatableLabelTranslation.php
@@ -8,7 +8,7 @@ use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
 /**
  * @ORM\Entity
  * @ORM\Table(name="bigfoot_translatable_label_translations",
- *     uniqueConstraints={@ORM\UniqueConstraint(name="lookup_unique_idx", columns={
+ *     uniqueConstraints={@ORM\UniqueConstraint(name="lookup_unique_idx_translation", columns={
  *         "locale", "object_id", "field"
  *     })}
  * )
