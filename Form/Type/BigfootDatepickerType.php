634878f5e68e510efb699258740d58a7eca591b9
Guillaume Manen <g.manen@c2is.fr>
2015-02-26 12:29:05 +0100

    Fixes datepicker localization
    Conflicts:
    
    	Resources/config/services.yml

---
+++ Form/Type/BigfootDatepickerType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Bigfoot\Bundle\CoreBundle\Form\Type;
+
+use Bigfoot\Bundle\ContextBundle\Service\ContextService;
+use Bigfoot\Bundle\CoreBundle\Form\DataTransformer\TagsToStringTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Extends the Symfony date type to use a localized date format automatically
+ *
+ * Class BigfootDatepickerType
+ * @package Bigfoot\Bundle\CoreBundle\Form\Type
+ */
+class BigfootDatepickerType extends AbstractType
+{
+    /** @var string */
+    protected $format;
+
+    /**
+     * @param array $localesConfiguration
+     * @param ContextService $context
+     */
+    public function __construct(array $localesConfiguration, ContextService $context)
+    {
+        $this->format = $localesConfiguration[$context->get('language')]['date_format'];
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'widget' => 'single_text',
+            'format' => $this->format,
+        ));
+    }
+
+    /**
+     * @return null|string|\Symfony\Component\Form\FormTypeInterface
+     */
+    public function getParent()
+    {
+        return 'date';
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'bigfoot_datepicker';
+    }
+}
