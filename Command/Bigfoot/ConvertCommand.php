8ae9afe4efa0ef30fe1d73a78ad6a9e5eda32c7e
Etienne Rouge <erouge@c2is.fr>
2015-03-20 11:06:30 +0100

    Adding convert command

---
+++ Command/Bigfoot/ConvertCommand.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Bigfoot\Bundle\CoreBundle\Command\Bigfoot;
+
+use Bigfoot\Bundle\CoreBundle\Command\BaseCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+class ConvertCommand extends BaseCommand
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('bigfoot:labels:convert')
+            ->setDescription('Convert SF2 translation files into BigFoot translation files')
+            ->addOption('no-vendor', null, InputOption::VALUE_NONE, 'Ne pas prendre les bundles dans le répertoire vendor/')
+            ->addOption('bundle', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Liste des bundles à convertir, si non spécifié tous les bundles seront convertis')
+            ->addOption('namespace', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Liste des namespaces à convertir, si non spécifié tous les namespaces seront convertis')
+            ->addOption('force', null, InputOption::VALUE_NONE, 'Effectue la convertion, sans cette option on ne fait rien')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $allBundles = $this->getContainer()->get('kernel')->getBundles();
+        $noVendor = $input->getOption('no-vendor');
+        $selectedBundles = $input->getOption('bundle');
+        $selectedNamespaces = $input->getOption('namespace');
+        $bundles = $allBundles;
+
+        // on ne peut pas donner une liste de bundles, et une liste de namespaces
+        if (count($selectedBundles) > 0 && count($selectedNamespaces) > 0) {
+            throw new \Exception('Vous ne pouvez pas indiquer une liste de bundles, et une liste de namespaces.');
+        }
+
+        // filtrage des bundles à convertir selon les options
+        if ($noVendor || count($selectedBundles) > 0 || count($selectedNamespaces) > 0) {
+            foreach ($allBundles as $bundle) {
+                $deleteBundle = false;
+                // pas les vendor
+                if ($noVendor && strpos($bundle->getPath(), '/vendor/') !== false) {
+                    $deleteBundle = true;
+                // liste de bundles donnée
+                } else if (count($selectedBundles) > 0 && in_array($bundle->getName(), $selectedBundles) == false) {
+                    $deleteBundle = true;
+                // liste de namespaces donnée
+                } else if (count($selectedNamespaces) > 0) {
+                    $bundleIsInNamespaces = false;
+                    foreach ($selectedNamespaces as $selectedNamespace) {
+                        if (substr($bundle->getNamespace(), 0, strlen($selectedNamespace)) == $selectedNamespace) {
+                            $bundleIsInNamespaces = true;
+                            break;
+                        }
+                    }
+                    $deleteBundle = !$bundleIsInNamespaces;
+                }
+
+                if ($deleteBundle) {
+                    unset($bundles[$bundle->getName()]);
+                }
+            }
+        }
+
+        // message d'info sur les bundles à convertir
+        if (count($bundles) == 0) {
+            throw new \Exception('<error>Aucun bundle à convertir. Vérifiez vos paramètres.</error>');
+        } else {
+            $output->writeln('Bundles à convertir : ' . implode(', ', array_keys($bundles)));
+        }
+
+        // convertion
+        if ($input->getOption('force')) {
+            $progress = $this->getHelper('progress');
+            $progress->start($output, count($bundles));
+            $this->getContainer()->get('bigfoot_core.translation.convert')->symfony2ToBigFoot($bundles, $progress);
+            $progress->finish();
+        // juste un message pour indiquer comment lancer la convertion
+        } else {
+            $output->writeln('Pour effectuer la convertion finale, il faut ajouter l\'option --force.');
+        }
+    }
+
+}
---
+++ Translation/Convert.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Bigfoot\Bundle\CoreBundle\Translation;
+
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Console\Helper\ProgressHelper;
+
+/**
+ * Gestion des convertions de traductions
+ */
+class Convert
+{
+
+    /** @var Kernel */
+    private $kernel = null;
+
+    /**
+     * @param Kernel $kernel
+     */
+    public function __construct(Kernel $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * Recherche récursive de tous les fichiers de traduction dans le répertoire donné
+     *
+     * @param string $dir
+     * @param array $translationFiles
+     */
+    protected function findTranslationFiles($dir, array &$translationFiles)
+    {
+        if (is_dir($dir) === false) {
+            return null;
+        }
+        foreach (scandir($dir) as $fileOrDir) {
+            $fullFileOrDir = $dir . DIRECTORY_SEPARATOR . $fileOrDir;
+            if (is_dir($fullFileOrDir)) {
+                if ($fileOrDir != '.' && $fileOrDir != '..') {
+                    $this->findTranslationFiles($fullFileOrDir, $translationFiles);
+                }
+            } else {
+                $pathinfo = pathinfo($fullFileOrDir);
+                if ($pathinfo['extension'] == 'yml') {
+                    $translationFiles[] = $fullFileOrDir;
+                }
+            }
+        }
+    }
+
+    /**
+     * Retourne la clef de traduction complète
+     *
+     * @param string $baseKey
+     * @param string $key
+     * @return string
+     */
+    protected function getTranslationKey($baseKey, $key)
+    {
+        return ($baseKey == null) ? $key : $baseKey . '.' . $key;
+    }
+
+    /**
+     * Parse un tableau de traductions et écrit dans $parsedTranslations sous la forme [foo.bar] => 'translation'
+     *
+     * @param string $key
+     * @param string $translations
+     * @param array $parsedTranslations
+     */
+    protected function parseTranslations($key, $translations, array &$parsedTranslations)
+    {
+        if (is_array($translations)) {
+            foreach ($translations as $translationKey => $translation) {
+                if (is_array($translation)) {
+                    $this->parseTranslations($this->getTranslationKey($key, $translationKey), $translation, $parsedTranslations);
+                } else {
+                    $parsedTranslations[$this->getTranslationKey($key, $translationKey)] = $translation;
+                }
+            }
+        } else {
+            $parsedTranslations[$key] = $translation;
+        }
+    }
+
+    /**
+     * Converti les fichiers de traductions "Symfony2 style" vers les fichiers "Bigfoot style"
+     *
+     * @param array $bundles
+     * @param ProgressHelper $progress
+     */
+    public function symfony2ToBigFoot(array $bundles = array(), $progress = null)
+    {
+        $newTranslationFiles = array();
+        $bundles = (count($bundles) == 0) ? $this->kernel->getBundles() : $bundles;
+
+        foreach ($bundles as $bundle) {
+            $translationsPath = $bundle->getPath() . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'translations';
+            $translationFiles = array();
+            $this->findTranslationFiles($translationsPath, $translationFiles);
+
+            foreach ($translationFiles as $translationFile) {
+                $pathinfo = pathinfo($translationFile);
+                $domain = substr($pathinfo['filename'], 0, strrpos($pathinfo['filename'], '.'));
+                $lang = substr($pathinfo['filename'], strrpos($pathinfo['filename'], '.') + 1);
+                $parsedTranslations = array();
+                $translations = Yaml::parse($translationFile);
+
+                // les clefs qui ont au moins 3 niveaux (par exemple foo.bar.baz) sont gérées dans un fichier foo.bar.yml
+                // les clefs qui ont moins de 3 niveaux sont gérées dans default.yml
+                foreach ($translations as $key1 => $translation1) {
+                    if (is_array($translation1)) {
+                        foreach ($translation1 as $key2 => $translation2) {
+                            $fileName = $this->getTranslationKey($key1, $key2) . '.yml';
+                            if (array_key_exists($fileName, $newTranslationFiles) == false) {
+                                $newTranslationFiles[$fileName] = array();
+                            }
+
+                            if (is_array($translation2)) {
+                                $parsedTranslations = array();
+                                $this->parseTranslations(null, $translation2, $parsedTranslations);
+
+                                foreach ($parsedTranslations as $parsedKey => $parsedTranslation) {
+                                    if (array_key_exists($parsedKey, $newTranslationFiles[$fileName]) == false) {
+                                        $newTranslationFiles[$fileName][$parsedKey] = array();
+                                        if ($domain != 'messages') {
+                                            $newTranslationFiles[$fileName][$parsedKey]['domain'] = $domain;
+                                        }
+                                        $newTranslationFiles[$fileName][$parsedKey]['value'] = array();
+                                    }
+
+                                    $newTranslationFiles[$fileName][$parsedKey]['value'][$lang] = $parsedTranslation;
+
+                                    // multiline
+                                    if (strpos($parsedTranslation, "\n")) {
+                                        $newTranslationFiles[$fileName][$parsedKey]['multiline'] = true;
+                                    }
+
+                                    // plural
+                                    if (strpos($parsedTranslation, "|")) {
+                                        $newTranslationFiles[$fileName][$parsedKey]['plural'] = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if ($progress instanceof ProgressHelper) {
+                $progress->advance();
+            }
+        }
+
+        // écriture des fichiers
+        foreach ($newTranslationFiles as $newTranslationFile => $newTranslations) {
+            $fileName = 'app' . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'translatable_label' . DIRECTORY_SEPARATOR . $newTranslationFile;
+            file_put_contents($fileName, Yaml::dump($newTranslations, 3));
+        }
+    }
+
+}
