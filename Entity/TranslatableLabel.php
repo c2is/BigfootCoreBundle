823ae8160a5d4fec4de7a4069c4011517e2c0278
Guillaume Prost <guillaume.prost0@gmail.com>
2016-03-16 18:03:15 +0100

    Add Validator not null for Translatable labels

--- Entity/TranslatableLabel.php
+++ Entity/TranslatableLabel.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Translation\MessageSelector;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Class TranslatableLabel
@@ -43,6 +44,7 @@ class TranslatableLabel
     /**
      * @var string
      *
+     * @Assert\NotNull()
      * @Gedmo\Translatable
      * @ORM\Column(name="value", type="text")
      */
