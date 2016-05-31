bf0a0fab48f48bc3764972fa10ba4d59bae824e7
John Daligault <j.daligault@c2is.fr>
2016-05-31 11:30:57 +0200

    testing service existance

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -128,12 +128,9 @@ class BaseController extends Controller
      */
     public function createForm($type, $data = null, array $options = array())
     {
-        try {
-            $typeObj = $this->get($type);
-            if (is_object($typeObj)) {
-                $type = get_class($typeObj);
-            }
-        } catch(\Exception $e) {}
+        if (is_string($type) && $this->container->has($type)) {
+            $type = get_class($this->get($type));
+        }
 
         if (!is_subclass_of($type, AbstractType::class)) {
             throw new InvalidArgumentException(sprintf('Expected argument of type "AbstractType" or fully qualified class name of a form type, got "%s"', is_string($type) ? $type : get_class($type)));
