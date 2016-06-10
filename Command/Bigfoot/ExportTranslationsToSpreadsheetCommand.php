afbf9a1ec94bbcc83f36efc33c6874a85d55aee0
Nicolas PHILIPPE <nikophil@gmail.com>
2016-06-10 18:09:57 +0200

    Create import translations command

--- Command/Bigfoot/ExportTranslationsToSpreadsheetCommand.php
+++ Command/Bigfoot/ExportTranslationsToSpreadsheetCommand.php
@@ -2,28 +2,23 @@
 
 namespace Bigfoot\Bundle\CoreBundle\Command\Bigfoot;
 
+use Bigfoot\Bundle\CoreBundle\Command\AbstractTranslationCommand;
 use Google\Spreadsheet\Batch\BatchRequest;
-use Google\Spreadsheet\DefaultServiceRequest;
-use Google\Spreadsheet\ServiceRequestFactory;
 use Google\Spreadsheet\Spreadsheet;
-use Google\Spreadsheet\SpreadsheetService;
 use Google\Spreadsheet\Worksheet;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Bigfoot\Bundle\CoreBundle\Command\BaseCommand;
 use Symfony\Component\Console\Exception\InvalidOptionException;
-use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
 use Symfony\Component\Yaml\Yaml;
 
 /**
- * Runs bigfoot:theme:install and assets:install
+ * Exports bigfoot translations to spreadsheet
  */
-class ExportTranslationsToSpreadsheetCommand extends BaseCommand
+class ExportTranslationsToSpreadsheetCommand extends AbstractTranslationCommand
 {
     /**
      * {@inheritdoc}
@@ -65,8 +60,11 @@ class ExportTranslationsToSpreadsheetCommand extends BaseCommand
 
         $availableLanguages = $this->getContainer()->getParameter('bigfoot_core.languages.front');
 
+        $index = 1;
         /** @var SplFileInfo $file */
         foreach ($finder as $file) {
+            $output->writeln(sprintf('<info>Handling translations %s [</info><comment>%s/%s</comment><info>]</info>', $file->getFilename(), $index++, $finder->count()));
+
             $fileName = explode('.', $file->getFilename());
             array_pop($fileName);
             $fileName = implode('.', $fileName);
@@ -76,66 +74,6 @@ class ExportTranslationsToSpreadsheetCommand extends BaseCommand
             $worksheet = $this->getWorksheetByName($output, $spreadsheet, $fileName, $availableLanguages);
             $this->proceedWorksheet($output, $worksheet, $translations, $availableLanguages, $fileName);
         }
-    }
-
-    /**
-     * @return SpreadsheetService
-     * @throws \Exception
-     */
-    private function getSpreadsheetService()
-    {
-        $oAuth          = $this->getContainer()->get('google_drive.oauth');
-        $token          = $oAuth->authenticate();
-        $serviceRequest = new DefaultServiceRequest($token);
-        ServiceRequestFactory::setInstance($serviceRequest);
-        $spreadsheetService = new SpreadsheetService();
-
-        return $spreadsheetService;
-    }
-
-    /**
-     * @param SpreadsheetService $spreadsheetService
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     * @return Spreadsheet
-     */
-    private function getSpreadsheet(
-        SpreadsheetService $spreadsheetService,
-        InputInterface $input,
-        OutputInterface $output
-    ) {
-        $spreadsheetFeed = $spreadsheetService->getSpreadsheets();
-        if (false === $this->getContainer()->hasParameter('bigfoot_core.translation.spreadsheet')) {
-            /** @var Spreadsheet $spreadsheet */
-            foreach ($spreadsheetFeed as $index => $listedSpreadsheet) {
-                $output->writeln('    [<info>' . $index . '</info>] ' . $listedSpreadsheet->getTitle());
-            }
-
-            $spreadsheetIndex = $this->getHelper('question')->ask(
-                $input,
-                $output,
-                new Question('Spreadsheet to chose :')
-            );
-            $output->writeln(sprintf('<info>spreadsheet chosen :</info> %s', $spreadsheetFeed[$spreadsheetIndex]->getTitle()));
-            $output->writeln(sprintf("<comment>You can define 'bigfoot_core.translation.spreadsheet' with '%s' to skip this step</comment>", $spreadsheetFeed[$spreadsheetIndex]->getId()));
-
-            $spreadsheet = $spreadsheetFeed[$spreadsheetIndex];
-        } else {
-            $spreadsheetId = $this->getContainer()->getParameter('bigfoot_core.translation.spreadsheet');
-            $spreadsheet   = null;
-            /** @var Spreadsheet $spreadsheet */
-            foreach ($spreadsheetFeed as $index => $listedSpreadsheet) {
-                if ($listedSpreadsheet->getId() == $spreadsheetId) {
-                    $spreadsheet = $listedSpreadsheet;
-                }
-            }
-
-            if (null === $spreadsheet) {
-                throw new Exception("Invalid argument under bigfoot_core.translation.spreadsheet key");
-            }
-        }
-
-        return $spreadsheet;
     }
 
     /**
@@ -164,6 +102,7 @@ class ExportTranslationsToSpreadsheetCommand extends BaseCommand
         if (count($spreadsheet->getWorksheets()) == 1 && count($worksheet->getCellFeed()->toArray()) == 0) {
             /** @var Worksheet $worksheet */
             $worksheet = $spreadsheet->getWorksheets()[0];
+            $worksheet->update($workSheetName, 2 + count($availableLanguages), 1);
         } else {
             $worksheet = $spreadsheet->addWorksheet($workSheetName, 1, 2 + count($availableLanguages));
         }
@@ -174,8 +113,8 @@ class ExportTranslationsToSpreadsheetCommand extends BaseCommand
         $cellFeed->editCell(1, $i++, "Translation label");
         $cellFeed->editCell(1, $i++, "Translation domain");
 
-        foreach ($availableLanguages as $language) {
-            $cellFeed->editCell(1, $i++, "Label in" . $language['label']);
+        foreach ($availableLanguages as $k => $language) {
+            $cellFeed->editCell(1, $i++, $k);
         }
         $output->write('    <info>Created !</info>
 ');
@@ -188,12 +127,19 @@ class ExportTranslationsToSpreadsheetCommand extends BaseCommand
      * @param Worksheet $worksheet
      * @param array $translations
      * @param array $availableLanguages
+     * @param string $workSheetName
      */
-    private function proceedWorksheet(OutputInterface $output, Worksheet $worksheet, $translations, $availableLanguages, $workSheetName)
-    {
-        $cellFeed                    = $worksheet->getCellFeed();
+    private function proceedWorksheet(
+        OutputInterface $output,
+        Worksheet $worksheet,
+        $translations,
+        $availableLanguages,
+        $workSheetName
+    ) {
+        $listFeed = $worksheet->getListFeed();
+
+        $cellFeed = $worksheet->getCellFeed();
         $translationsInWorksheetTemp = $cellFeed->toArray();
-        $rowCount                    = count($translationsInWorksheetTemp);
 
         $translationsInWorksheet = array();
         foreach ($translationsInWorksheetTemp as $k => $trans) {
@@ -205,30 +151,29 @@ class ExportTranslationsToSpreadsheetCommand extends BaseCommand
         }
 
         $translationsToWriteInWorksheet = array_diff_key($translations, $translationsInWorksheet);
-        $batchRequest                   = new BatchRequest();
 
-        $worksheet->update($workSheetName, 2 + count($availableLanguages), $rowCount + count($translationsToWriteInWorksheet));
+        $output->writeln(sprintf('<comment>Writing worksheet %s (</comment><info>%s translations to write</info><comment>)...</comment>', $workSheetName, count($translationsToWriteInWorksheet)));
 
-        $progressBar = new ProgressBar($output, count($translationsToWriteInWorksheet));
-        $progressBar->start();
-        $progressBar->setFormat('very_verbose');
+        if (count($translationsToWriteInWorksheet) > 0) {
+            $progressBar = new ProgressBar($output, count($translationsToWriteInWorksheet));
+            $progressBar->start();
 
-        foreach ($translationsToWriteInWorksheet as $transLabel => $trans) {
-            $batchRequest->addEntry($cellFeed->createInsertionCell(++$rowCount, 1, $transLabel));
-            $batchRequest->addEntry($cellFeed->createInsertionCell($rowCount, 2, $trans['domain']));
-            foreach ($availableLanguages as $language) {
-                if (isset($trans['value'][$language['value']])) {
-                    $index = array_flip(array_keys($availableLanguages))[$language['value']];
-                    $label = $trans['value'][$language['value']];
-                    $label = str_replace('<', '&lt;', $label);
-                    $label = str_replace('>', '&gt;', $label);
-                    $batchRequest->addEntry($cellFeed->createInsertionCell($rowCount, 3 + $index, $label));
+            foreach ($translationsToWriteInWorksheet as $transLabel => $trans) {
+                $row = [];
+                $row['translationlabel'] = $transLabel;
+                $row['translationdomain'] = $trans['domain'];
+                foreach ($availableLanguages as $language) {
+                    if (isset($trans['value'][$language['value']])) {
+                        $label = $trans['value'][$language['value']];
+                        $row[$language['value']] = $label;
+                    }
                 }
+                $listFeed->insert($row);
+                $progressBar->advance();
             }
-            $progressBar->advance();
+            $progressBar->finish();
+            $output->writeln('');
         }
-        $cellFeed->insertBatch($batchRequest);
-        $progressBar->finish();
         $output->writeln('');
     }
 }
